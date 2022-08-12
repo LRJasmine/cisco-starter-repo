2cdc7f28e12162ae5b15826c816d01b5fe09d1e6
Replit user <>
2022-08-12 04:14:14 +0000

    adding navbar

---
+++ sextant/src/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav className="navbar bg-teal-900 flex flex-row">
+      <div className="navtitle">
+        <p className="navttiletext text-white font-bold">Sextant</p>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
